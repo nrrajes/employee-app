d890c65f3867d8b837f7a05ad2aaf502456334e4
Rajesh NR <amicale.rajesh@outlook.com>
2026-08-06 00:35:59 +0530

    Fix CI test without database dependency

--- src/test/java/com/nr/devops/employeeapp/EmployeeAppApplicationTests.java
+++ src/test/java/com/nr/devops/employeeapp/EmployeeAppApplicationTests.java
@@ -1,13 +1,11 @@
 package com.nr.devops.employeeapp;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class EmployeeAppApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
